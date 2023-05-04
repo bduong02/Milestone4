40b555939c494410e3900799f15a5d708891e5e0
Sothaninn Sieng <94101972+Sothaninn@users.noreply.github.com>
2023-05-03 21:38:07 -0700

    Update SQLExec.cpp

--- SQLExec.cpp
+++ SQLExec.cpp
@@ -1,6 +1,6 @@
 /**
  * @file SQLExec.cpp - implementation of SQLExec class
- * @author Bryan Duong, Ninn Sieng
+ * @author Bryan Duong, Sothaninn Sieng
  */
 #include "SQLExec.h"
 
@@ -53,7 +53,6 @@ QueryResult::~QueryResult() {
     }
 }
 
-
 QueryResult *SQLExec::execute(const SQLStatement *statement) {
     // checking if tables have been initialized
     // and creating tables if not
@@ -98,7 +97,7 @@ void SQLExec::column_definition(const ColumnDefinition *col, Identifier &column_
             throw SQLExecError("not implemented");
    }    
 }
-
+// CREATE ...
 QueryResult *SQLExec::create(const CreateStatement *statement) {
     switch(statement->type)
     {
@@ -123,16 +122,82 @@ QueryResult *SQLExec::drop(const DropStatement *statement) {
             return new QueryResult("not implemented");
     }
 }
-
+// SHOW ...
 QueryResult *SQLExec::show(const ShowStatement *statement) {
-    return new QueryResult("not implemented"); // FIXME
+    switch (statement-> type)
+    {
+        case ShowStatement::kIndex:
+            return show_index(statement);
+        case ShowStatement::kTables:
+            return show_tables();
+        case ShowStatement::kColumns:
+            return show_columns(statement);
+        default:
+            return new QueryResult("not implemented");
 }
-
+//Show the tables in _tables table
 QueryResult *SQLExec::show_tables() {
-    return new QueryResult("not implemented"); // FIXME
-}
+    ColumnNames *col_names = new ColumnNames;
+    col_names->push_back("table_name");
+    ColumnAttributes *col_attrib = new ColumnAttributes;
+    col_attrib->push_back(ColumnAttribute(ColumnAttribute::TEXT));
+    
+    //Select all the record of _tables table 
+    Handles *handles = SQLExec::tables->select();
+    //Number of rows excluding the name of the _tables table, _columns tables and _indices table
+    u_long n = handles->size() - 3;
 
+    //For each record in _tables table, extract row data(row_dict) by column names
+    ValueDicts *rows = new ValueDicts;
+    for (auto const &handle: *handles) {
+        ValueDict *row_dict = SQLExec::tables->project(handle, col_names);
+        Identifier table_name = row_dict->at("table_name").s;
+        //Don't show if table_name is the name of the _tables table, _columns table or _indices table, 
+        if (table_name != Tables::TABLE_NAME && table_name != Columns::TABLE_NAME && table_name != Indices::TABLE_NAME)
+            rows->push_back(row_dict);
+        else
+            delete row_dict;
+    }
+    delete handles;
+    return new QueryResult(col_names, col_attrib, rows, "successfully returned " + to_string(n) + " rows");
+}
+//Show the columns in _columns table
 QueryResult *SQLExec::show_columns(const ShowStatement *statement) {
-    return new QueryResult("not implemented"); // FIXME
+    //Get _columns table 
+    DbRelation &columns = SQLExec::tables->get_table(Columns::TABLE_NAME);
+
+    ColumnNames *col_names = new ColumnNames;
+    col_names->push_back("table_name");
+    col_names->push_back("column_name");
+    col_names->push_back("data_type");
+    ColumnAttributes *col_attrib = new ColumnAttributes;
+    col_attrib->push_back(ColumnAttribute(ColumnAttribute::TEXT));
+
+    ValueDict where;
+    where["table_name"] = Value(statement->tableName);
+    //Select from the _columns table where table_name = tableName in statement
+    Handles *handles = columns.select(&where);
+    //number of rows returned
+    u_long n = handles->size();
+    
+    //For each record in _columns table, extract row data(row_dict) by table_name, column_name and data_type
+    ValueDicts *rows = new ValueDicts;
+    for (auto const &handle: *handles) {
+        ValueDict *row_dict = columns.project(handle, col_names);
+        rows->push_back(row_dict);
+    }
+    delete handles;
+    return new QueryResult(col_names, col_attrib, rows, "successfully returned " + to_string(n) + " rows");
 }
 
+static QueryResult *SQLExec::create_index(const hsql::CreateStatement *statement){
+    return new QueryResult("not implemented");
+}
+    
+static QueryResult *SQLExec::drop_index(const hsql::DropStatement *statement){
+    return new QueryResult("not implemented");
+}
+
+QueryResult *SQLExec::show_index(const hsql::ShowStatement *statement){
+    return new QueryResult("not implemented");
+}
